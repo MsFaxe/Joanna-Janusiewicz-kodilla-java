2367fdb5fad2829fa8ce0a651c0761dc153e5e88
MsFaxe <anty.wjj@gmail.com>
2020-06-07 15:49:36 +0200

    14.2

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -0,0 +1,36 @@
+package com.kodilla.spring.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public final class Library {
+    private final List<String> books = new ArrayList<>();
+
+    //3rd injection method - field
+//    @Autowired
+    private LibraryDbController libraryDbController;
+
+    //1st injection method - constructor
+    @Autowired
+    public Library(LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+
+    //2nd injection method - setter
+//    @Autowired
+//    public void setLibraryDbController(LibraryDbController libraryDbController) {
+//        this.libraryDbController = libraryDbController;
+//    }
+
+    public void saveToDb() {
+        libraryDbController.saveData();
+    }
+
+    public void loadFromDb() {
+        libraryDbController.loadData();
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.library;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class LibraryTestSuite {
+    @Test
+    public void testLoadFromDb() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.loadFromDb();
+        //Then
+        //
+    }
+
+    @Test
+    public void testSaveToDB() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+        //When
+        library.saveToDb();
+        //Then
+        //
+    }
+}
