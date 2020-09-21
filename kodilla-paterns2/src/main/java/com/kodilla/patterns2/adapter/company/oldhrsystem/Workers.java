8c8cecce6d4d44caa9b1157b311ccf10f3416a53
MsFaxe <anty.wjj@gmail.com>
2020-09-21 16:47:46 +0200

    24.5

--- kodilla-paterns2/src/main/java/com/kodilla/patterns2/adapter/company/oldhrsystem/Workers.java
+++ kodilla-paterns2/src/main/java/com/kodilla/patterns2/adapter/company/oldhrsystem/Workers.java
@@ -1,4 +1,30 @@
 package com.kodilla.patterns2.adapter.company.oldhrsystem;
 
 public class Workers {
+    private String[][] workers = {
+            {"12345678920", "John", "Smith"},
+            {"14785236987", "Ivone", "Novak"},
+            {"98745632140", "Jessie", "Pinkman"},
+            {"78965412321", "Walter", "White"},
+            {"45687745561", "Clara", "Lanson"}};
+    private double[] salaries = {
+            4500.00,
+            3700.00,
+            4350.00,
+            9000.00,
+            6200.00};
+    public String getWorker(int n) {
+        if(n> salaries.length) {
+            return "";
+        }
+        return workers[n][0] + ", " + workers[n][1] + ", " + workers[n][2] + ", " + salaries[n];
+    }
+
+    public String[][] getWorkers() {
+        return workers;
+    }
+
+    public double[] getSalaries() {
+        return salaries;
+    }
 }
