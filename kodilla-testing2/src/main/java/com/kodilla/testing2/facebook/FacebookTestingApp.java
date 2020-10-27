fee1bf80b7890e95d047dcab4935ad8a6235f038
MsFaxe <anty.wjj@gmail.com>
2020-10-27 13:56:45 +0100

    31.3 - zadanie

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -9,20 +9,17 @@ import org.openqa.selenium.support.ui.Select;
 public class FacebookTestingApp {
     public static final String XPATH_ACCEPT_COOKIE = "//*[@id=\"u_0_k\"]";
     public static final String XPATH_CREATE_ACC = "//div[contains(@class, \"_6ltg\")]/a[contains(@role, \"button\")]";
-    public static final String XPATH_NAME = "//*[@id=\"u_1_b\"]";
-           /* "//*[@id=\"u_2_b\"]"; //*[@id="u_2_b"]*/
-    public static final String XPATH_LASTNAME = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input";
-    public static final String XPATH_MAIL = "reg_email__";
-    public static final String XPATH_PASS = "reg_passwd__";
-    public static final String XPATH_BIRTH_DAY = "birhtday_day";
-    public static final String XPATH_BIRTH_MONTH = "birhtday_month";
-    public static final String XPATH_BIRTH_YEAR = "birhtday_year";
-    public static final String XPATH_SEX = "u_2_2";
+    public static final String XPATH_NAME = "//*[@name=\"firstname\"]";
+    public static final String XPATH_LASTNAME = "//*[@name=\"lastname\"]";
+    public static final String XPATH_MAIL = "//*[@name=\"reg_email__\"]";
+    public static final String XPATH_CONFIRM_MAIL = "//*[@name=\"reg_email_confirmation__\"]";
+    public static final String XPATH_PASS = "//*[@name=\"reg_passwd__\"]";
+    public static final String XPATH_BIRTH_DAY = "//*[@id=\"day\"]";
+    public static final String XPATH_BIRTH_MONTH = "//*[@id=\"month\"]";
+    public static final String XPATH_BIRTH_YEAR = "//*[@id=\"year\"]";
+    public static final String XPATH_SEX = "//span[@data-type=\"radio\"]//input[@value=2]";
 
-    public static final String XPATH_WAIT_FOR = "//*[@id=\"facebook\"]/body/div[3]/div[1]";
-        /*"//*[@type=\"submit\"]";*/
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com");
 
@@ -33,18 +30,27 @@ public class FacebookTestingApp {
         WebElement searchField = driver.findElement(By.xpath(XPATH_CREATE_ACC));
         searchField.click();
 
-        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
+        Thread.sleep(500);
 
-        driver.findElement(By.xpath(XPATH_NAME));
-//        WebElement firstnameField = driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input"));
-//        firstnameField.click();
-//        firstnameField.sendKeys("John");
-//        firstnameField.submit();
+        driver.findElement(By.xpath(XPATH_NAME)).sendKeys("John");
+        driver.findElement(By.xpath(XPATH_LASTNAME)).sendKeys("Kowalski");
+        driver.findElement(By.xpath(XPATH_MAIL)).sendKeys("John@xyz.com");
 
-        //while (!driver.findElement(By.name(XPATH_BIRTH_DAY)).isDisplayed());
+        Thread.sleep(500);
 
-//        WebElement selectBirthDay = driver.findElement(By.name(XPATH_BIRTH_DAY));
-//        Select selectBoard = new Select(selectBirthDay);
-//        selectBoard.selectByIndex(1);
+        driver.findElement(By.xpath(XPATH_CONFIRM_MAIL)).sendKeys("John@xyz.com");
+        driver.findElement(By.xpath(XPATH_PASS)).sendKeys("password");
+
+        WebElement selectBirthDay = driver.findElement(By.xpath(XPATH_BIRTH_DAY));
+        Select selectDay = new Select(selectBirthDay);
+        selectDay.selectByIndex(5);
+        WebElement selectBirthMonth = driver.findElement(By.xpath(XPATH_BIRTH_MONTH));
+        Select selectMonth = new Select(selectBirthMonth);
+        selectMonth.selectByIndex(2);
+        WebElement selectBirthYear = driver.findElement(By.xpath(XPATH_BIRTH_YEAR));
+        Select selectYear = new Select(selectBirthYear);
+        selectYear.selectByIndex(20);
+
+        driver.findElement(By.xpath(XPATH_SEX)).click();
     }
 }
